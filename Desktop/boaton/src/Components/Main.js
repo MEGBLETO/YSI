12edd87f1566a14276c64f05f0063e55987b618f
carnel MEGBLETO <carnelmegbleto@carnels-MacBook-Pro.local>
2022-01-22 13:11:33 +0100

    sideNav

--- Desktop/boaton/src/Components/Main.js
+++ Desktop/boaton/src/Components/Main.js
@@ -4,7 +4,7 @@ import Containt from './Containt';
 
 const Main = () => {
   return <div className="relative top-20 h-full">
-      <div className="grid grid-cols-2">
+      <div className="grid-col-1 sm:grid grid-cols-2 p-2">
           <div className="hidden sm:block w-1/2"><Sidenav/></div>
           <div className="col-span-1"><Containt/></div>
       </div>
