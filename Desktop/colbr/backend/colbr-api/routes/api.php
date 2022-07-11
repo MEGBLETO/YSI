170a3076ba09d435d3bb041202d9d75a999268fe
MEGBLETO <carnelilordwiz@gmail.com>
2022-07-11 13:04:16 +0200

    temp

--- Desktop/colbr/backend/colbr-api/routes/api.php
+++ Desktop/colbr/backend/colbr-api/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+
+
+Route::post('/inscription', [userController::class, 'createUser']);
+Route::post('/connexion', [userController::class, 'connectUser']);
+
+
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
